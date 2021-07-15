0c2b237bb2141d4d4e7225666acd16eb3e62959e
Danill Yudhistia Setiawan <danillyudhistira@gmail.com>
2021-07-15 08:40:25 +0700

    add comment

--- microservices-app/src/app.controller.ts
+++ microservices-app/src/app.controller.ts
@@ -2,7 +2,11 @@ import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 import { User } from './database/model/user.entity';
-
+/* 
+  microservice using rabbitmq 
+  structure : client -> microservice-client
+              provider -> microservice-app
+*/
 @Controller()
 export class AppController {
   constructor(private readonly service: AppService) {}
